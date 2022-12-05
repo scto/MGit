32a206af926667f371a4034c6f9f42a623190a04
deenu143 <86369518+Deenu143@users.noreply.github.com>
2022-12-05 19:59:57 +0530

    Update MGitApplication.kt

--- app/src/main/java/me/sheimi/sgit/MGitApplication.kt
+++ app/src/main/java/me/sheimi/sgit/MGitApplication.kt
@@ -6,10 +6,10 @@ import android.content.Context
 import me.sheimi.android.utils.SecurePrefsException
 import me.sheimi.android.utils.SecurePrefsHelper
 import me.sheimi.sgit.preference.PreferenceHelper
-import org.acra.config.dialog
-import org.acra.config.mailSender
-import org.acra.data.StringFormat
-import org.acra.ktx.initAcra
+//import org.acra.config.dialog
+//import org.acra.config.mailSender
+//import org.acra.data.StringFormat
+//import org.acra.ktx.initAcra
 //import org.bouncycastle.jce.provider.BouncyCastleProvider
 //import org.conscrypt.Conscrypt
 import org.eclipse.jgit.transport.CredentialsProvider
@@ -54,36 +54,12 @@ open class MGitApplication : Application() {
             securePrefsHelper = SecurePrefsHelper(this)
             mCredentialsProvider = AndroidJschCredentialsProvider(securePrefsHelper)
         } catch (e: SecurePrefsException) {
-            Timber.e(e)
+         //   Timber.e(e)
         }
     }
 
     override fun attachBaseContext(base:Context) {
         super.attachBaseContext(base)
 
-        initAcra {
-            //core configuration:
-            buildConfigClass = BuildConfig::class.java
-            reportFormat = StringFormat.JSON
-            // each plugin you chose above can be configured in a block like this:
-            dialog {
-                text = getString(R.string.dialog_error_send_report)
-                //opening this block automatically enables the plugin.
             }
-            mailSender {
-                withMailTo(getString(R.string.crash_report_email))
-            }
-        }
     }
-
-    private fun setAppVersionPref() {
-        val sharedPreference = getSharedPreferences(
-            getString(R.string.preference_file_key),
-            Context.MODE_PRIVATE)
-        val version = BuildConfig.VERSION_NAME
-        sharedPreference
-            .edit()
-            .putString(getString(R.string.preference_key_app_version), version)
-            .apply()
-    }
-}

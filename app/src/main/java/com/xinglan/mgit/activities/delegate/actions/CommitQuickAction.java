0c179c4932b3e0201ec71cd190677c22896d4ad6
XINGLAN <32425955+Zacharia2@users.noreply.github.com>
2024-08-25 22:16:45 +0800

    CommitQuickAction step 1/2

---
+++ app/src/main/java/com/xinglan/mgit/activities/delegate/actions/CommitQuickAction.java
@@ -0,0 +1,19 @@
+package com.xinglan.mgit.activities.delegate.actions;
+
+import com.xinglan.mgit.activities.RepoDetailActivity;
+import com.xinglan.mgit.database.models.Repo;
+
+public class CommitQuickAction extends RepoAction {
+
+    public CommitQuickAction(Repo repo, RepoDetailActivity activity) {
+        super(repo, activity);
+    }
+
+    @Override
+    public void execute() {
+
+    }
+
+}
+
+

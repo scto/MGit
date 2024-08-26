edb6e48f003c0eac64b333ded7a3e806f3370f4d
XINGLAN <32425955+Zacharia2@users.noreply.github.com>
2024-08-26 08:36:30 +0800

    Update CommitQuickAction.java

--- app/src/main/java/com/xinglan/mgit/activities/delegate/actions/CommitQuickAction.java
+++ app/src/main/java/com/xinglan/mgit/activities/delegate/actions/CommitQuickAction.java
@@ -15,6 +15,17 @@ public class CommitQuickAction extends RepoAction {
 
     @Override
     public void execute() {
+        /*
+         * index.lock的作用：防止在当前运行的git过程以外去更改本地存储库的资源。
+         * 避免了多个git进程同时进行操作更改导致问题！
+         *
+         * 每当你运行一个git进程时，git就会在.git目录创建一个index.lock文件。
+         * 例如，在当前的git仓库里运行git add .来stage本地的修改点，git就会在git add执行的时候创建index.lock文件，
+         * 命令执行结束后，删除该文件。
+         *
+         * 如果某个进程退出/结束的时候除了问题，可能会导致index.lock文件没有被清除掉，此时，你需要将index.lock文件
+         * 手动移除
+         */
         AddToStageTask stageAndCommit = new AddToStageTask(mRepo, ".", isSuccess -> {
             RepoAction commit = new CommitAction(mRepo, mActivity);
             commit.execute();

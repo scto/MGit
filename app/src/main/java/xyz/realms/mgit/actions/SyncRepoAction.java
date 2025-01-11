3654d2f7e4b2b6933a1e626983b753076e158bb6
XINGLAN <32425955+Zacharia2@users.noreply.github.com>
2025-01-11 13:48:09 +0800

    update idea

--- app/src/main/java/xyz/realms/mgit/actions/SyncRepoAction.java
+++ app/src/main/java/xyz/realms/mgit/actions/SyncRepoAction.java
@@ -20,24 +20,26 @@ public class SyncRepoAction extends RepoAction {
     public void execute() {
         /*
          * 本地有修改，远程无修改 -> add暂存、commit提交、push推送
-         * 本地有修改、远程有修改 -> 暂存、拉取、恢复暂存、提交、推送
          * 本地无修改，远程有修改 -> 拉取
          * 本地无修改，远程无修改 -> 提示全部最新
          * AddAllAction、CommitAction
+         * 本地有修改、远程有修改，极易产生冲突。暂存、拉取、在恢复暂存的时候有些文件版本不一样了必然导致冲突。
          */
         try {
             // 获取当前仓库的状态
             Status status = mRepo.getGit().status().call();
             // 检查是否有更改
             if (status.hasUncommittedChanges()) {
-                AddToStageTask addTask = new AddToStageTask(mRepo, ".", (isAddToStageTaskSuccess) -> {
-                    new CheckoutFileTask(mRepo, null, (isCheckoutFileTaskSuccess) -> {
-                        new RebaseTask(mRepo, "true", (isRebaseTaskSuccess) -> {
-                            new RebaseTask(mRepo, "true", null).execute();
-                        }).execute();
-                    }).execute();
-                });
-                addTask.executeTask();
+//                不能通过action进行链式调用，即使不能重用action的代码。
+//                只能通过task进行链式调用。
+//                AddToStageTask addTask = new AddToStageTask(mRepo, ".", (isAddToStageTaskSuccess) -> {
+//                    new CheckoutFileTask(mRepo, null, (isCheckoutFileTaskSuccess) -> {
+//                        new RebaseTask(mRepo, "true", (isRebaseTaskSuccess) -> {
+//                            new RebaseTask(mRepo, "true", null).execute();
+//                        }).execute();
+//                    }).execute();
+//                });
+//                addTask.executeTask();
             } else {
                 mActivity.showToastMessage(R.string.alert_uncommitted_changes);
             }

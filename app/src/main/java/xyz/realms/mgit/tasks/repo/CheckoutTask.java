bff184838e98a130ddec95351d412a8a6ed018a5
XINGLAN <32425955+Zacharia2@users.noreply.github.com>
2025-01-19 18:33:44 +0800

    jgit7不支持稀疏检出

--- app/src/main/java/xyz/realms/mgit/tasks/repo/CheckoutTask.java
+++ app/src/main/java/xyz/realms/mgit/tasks/repo/CheckoutTask.java
@@ -1,5 +1,7 @@
 package xyz.realms.mgit.tasks.repo;
 
+import android.widget.Toast;
+
 import org.eclipse.jgit.api.CreateBranchCommand;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.api.errors.JGitInternalException;
@@ -12,7 +14,10 @@ import org.eclipse.jgit.treewalk.TreeWalk;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
+import xyz.realms.mgit.MGitApplication;
+import xyz.realms.mgit.R;
 import xyz.realms.mgit.database.Repo;
 import xyz.realms.mgit.errors.StopTaskException;
 
@@ -27,6 +32,7 @@ public class CheckoutTask extends RepoOpTask {
         mCallback = callback;
         mCommitName = name; //refs/heads/master
         mBranch = branch;
+        setSuccessMsg(R.string.success_checkout_file);
     }
 
     @Override
@@ -42,9 +48,9 @@ public class CheckoutTask extends RepoOpTask {
     }
 
     public boolean checkout(String name, String newBranch) {
-        List<String> checkoutFiles = new ArrayList<>();
+        // 将大于50MB的文件添加到checkoutFiles列表。
+        List<String> largeFiles = new ArrayList<>();
         try {
-            // 将小于50MB的文件添加到checkoutFiles列表中等待稀疏检出。
             Repository repository = mRepo.getGit().getRepository();
             RevWalk revPool = new RevWalk(repository);
             ObjectReader reader = revPool.getObjectReader();
@@ -54,11 +60,15 @@ public class CheckoutTask extends RepoOpTask {
             while (treeWalk.next()) {
                 ObjectLoader ol = reader.open(treeWalk.getObjectId(0));
                 long fileSize = ol.getSize();
-                if (fileSize <= 50 * 1024 * 1024) { // 文件大小小于等于50MB
-                    checkoutFiles.add(treeWalk.getPathString());
+                if (fileSize > 50 * 1024 * 1024) { // 文件大小小于等于50MB
+                    largeFiles.add(treeWalk.getPathString());
                 }
             }
-
+            if (!largeFiles.isEmpty()) {
+                Toast.makeText(MGitApplication.getContext(), String.format(Locale.getDefault(),
+                    "存在%d个文件超过50MB，无法检出, 暂不支持此类仓库!", largeFiles.size()), Toast.LENGTH_SHORT).show();
+                return false;
+            }
             if (name == null) {
                 checkoutNewBranch(newBranch);
             } else {
@@ -66,8 +76,7 @@ public class CheckoutTask extends RepoOpTask {
                     checkoutFromRemote(name, newBranch == null || newBranch.isEmpty() ?
                         Repo.getCommitName(name) : newBranch);
                 } else if (newBranch == null || newBranch.isEmpty()) {
-                    //在克隆远程仓库时，只checkout小于50mb的文件。
-                    checkoutFromLocal(name, checkoutFiles);
+                    checkoutFromLocal(name);
                 } else {
                     checkoutFromLocal(name, newBranch);
                 }
@@ -82,33 +91,23 @@ public class CheckoutTask extends RepoOpTask {
         return true;
     }
 
-    public void checkoutNewBranch(String name) throws GitAPIException,
-        JGitInternalException, StopTaskException {
+    public void checkoutNewBranch(String name) throws GitAPIException, JGitInternalException,
+        StopTaskException {
         mRepo.getGit().checkout().setName(name).setCreateBranch(true).call();
     }
 
-    public void checkoutFromLocal(String name, List<String> checkoutFiles)
-        throws GitAPIException, JGitInternalException, StopTaskException {
-        //在克隆远程仓库时，只checkout小于50mb的文件。
-        if (checkoutFiles.isEmpty()) return;
-        mRepo.getGit().checkout().addPaths(checkoutFiles).setStartPoint(name).call();
+    public void checkoutFromLocal(String name) throws GitAPIException, JGitInternalException,
+        StopTaskException {
+        mRepo.getGit().checkout().setStartPoint(name).call();
     }
 
     public void checkoutFromLocal(String name, String branch) throws GitAPIException,
         JGitInternalException, StopTaskException {
-        mRepo.getGit().checkout().setCreateBranch(true).setName(branch)
-            .setStartPoint(name).call();
+        mRepo.getGit().checkout().setCreateBranch(true).setName(branch).setStartPoint(name).call();
     }
 
-    public void checkoutFromRemote(String remoteBranchName, String branchName)
-        throws GitAPIException, JGitInternalException, StopTaskException {
-        mRepo.getGit().checkout().setCreateBranch(true).setName(branchName)
-            .setStartPoint(remoteBranchName).call();
-        mRepo.getGit()
-            .branchCreate()
-            .setUpstreamMode(
-                CreateBranchCommand.SetupUpstreamMode.SET_UPSTREAM)
-            .setStartPoint(remoteBranchName).setName(branchName)
-            .setForce(true).call();
+    public void checkoutFromRemote(String remoteBranchName, String branchName) throws GitAPIException, JGitInternalException, StopTaskException {
+        mRepo.getGit().checkout().setCreateBranch(true).setName(branchName).setStartPoint(remoteBranchName).call();
+        mRepo.getGit().branchCreate().setUpstreamMode(CreateBranchCommand.SetupUpstreamMode.SET_UPSTREAM).setStartPoint(remoteBranchName).setName(branchName).setForce(true).call();
     }
 }
